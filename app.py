5b476eaf6c044c721e0415e93ce61ffeb7416223
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-27 02:14:36 +0300

    users can now sign in

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def register():
 	return render_template("register.html")
 
+@app.route("/sign-in")
+def sign_in():
+	return render_template("sign_in.html")
+
 if __name__ == "__main__":
 	app.run(debug=True) 
---
+++ static/js/sign_in.js
@@ -0,0 +1,49 @@
+setTitle("Sign in | FireChat");
+
+addNewLine();
+
+addHeading("Sign in", 6, "center");
+
+addNewLine();
+
+let usernameField = addInput("text", "Username");
+let passwordField = addInput("password", "Password");
+
+const inputFields = [usernameField, passwordField];
+inputFields.forEach((inputField) => {
+	inputField.position = "center";
+	inputField.classes = "form-input";
+	inputField.update();
+});
+
+let signInButton = addButton("register", "primary", "center");
+signInButton.onclick = () => {
+	if (checkCookie("username")) {
+        alert("You are already signed in!");
+        window.location.replace("/");
+    }
+
+	let username = usernameField.element.value;
+	let password = passwordField.element.value;
+
+    if (username == "" || password == "") {
+        alert("You must fill in all input fields!")
+    } else {
+        firebase.database().ref("/users/" + username).on("value", function(snapshot) {
+            if (!snapshot.val()) {
+                alert("Username or password is incorrect. Try again.");
+            } else {
+                if (snapshot.val()["Password"] == password) {
+                    setCookie("username", username, 100);
+                    setCookie("name", snapshot.val()["Name"], 100);
+					window.location.replace("/");
+                } else {
+                    alert("Username or password is incorrect. Try again.");
+                }
+            }
+        });
+    }
+};
+signInButton.update();
+
+addStyle(".form-input { margin-bottom: 2vh !important; }");
