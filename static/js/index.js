2ed9726bc2bde5e8b297a64b0ed7a96237e934a9
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-07-12 18:08:48 +0500

    added recent chatrooms

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,44 @@
-setTitle("Welcome to FireChat");
+let chatrooms_list = [];
 
-let heading;
+let display_chatrooms = () => {
+	remove_chatrooms();
+	let heading = addHeading("Recent Chatrooms", 5);
+	heading.setStyle("margin-left: 5%;");
+	firebase.database().ref("/users/" + getCookie("username")).on("value", snapshot => {
+		const chatrooms = snapshot.val()["Chatrooms"];
+		const number_of_chatrooms = snapshot.val()["Number_of_chatrooms"];
 
-if (!checkCookie("username")) {
-	heading = addHeading("Welcome to FireChat", 6, "center");
-} else {
-	heading = addHeading("Hello, " + getCookie("name") + "!", 6, "center");
+		for (let i = number_of_chatrooms; i > 0; i--) {
+			const chatroom = chatrooms["chatroom" + i];
+			if (chatroom) {
+				if (!chatrooms_list.includes(chatroom["Id"])) {
+					let chatroom_link = addLink(chatroom["Name"], "chat-" + chatroom["Id"]);
+					chatroom_link.setStyle("margin-left: 10%;");
+					chatrooms_list = [...chatrooms_list, chatroom["Id"]];
+					addNewLine();
+				}
+			}
+		}
+		addHTML("<hr>");
+	});
 }
 
-heading.setStyle("margin-top: 20vh");
+let remove_chatrooms = () => {
+	chatrooms_list.forEach(chatroom => {
+		chatroom.remove();
+	});
+	chatrooms_list = [];
+}
+
+let heading;
+if (!checkCookie("username")) {
+	setTitle("Welcome to FireChat");
+	heading = addHeading("Welcome to FireChat", 6, "center");
+	heading.setStyle("margin-top: 20vh");
+} else {
+	setTitle("Recent Chatrooms | FireChat");
+	addNewLine();
+	heading = addHeading("Hello, " + getCookie("name") + "!", 6, "center");
+	addHTML("<hr>");
+	display_chatrooms();
+}
